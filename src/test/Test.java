7a0b6e8b94d6d7dc2b325b3b0fcff308b0aba0b7
grzesiek888 <grzegorzmacurek@wp.pl>
2017-05-16 17:57:42 +0200

    test

---
+++ src/test/Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Grzegorz
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+    
+    private static void pierwszaFunkcja(){
+        System.out.println("pierwsza funkcja");
+    }
+}
